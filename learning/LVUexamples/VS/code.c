d6a77116fb5346a1ca87faf1ec1aa51e2f60a719
Jajukym <jajukym@gmail.com>
2020-02-21 12:03:29 -0700

    controller will test passthrough with pulse...

---
+++ learning/LVUexamples/VS/code.c
@@ -0,0 +1,13 @@
+/* Call Library source file */
+
+#include "extcode.h"
+
+int32_t Multiply(int32_t arg1, int32_t arg2);
+
+int32_t Multiply(int32_t arg1, int32_t arg2)
+{
+
+	/* Insert code here */
+
+}
+
